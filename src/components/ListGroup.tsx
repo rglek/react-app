6e773331a55751792687c34229ca4db384f2418e
Rost Glek <Rost.Glek@islandhealth.ca>
2024-02-01 18:18:10 -0800

    listGroup upd

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,19 @@
+import { MouseEvent } from "react";
+
 const ListGroup = () => {
   let items = ["New York", "London", "Paris", "Tokio", "Sydney"];
-  //   items = [];
-  //   const getMessage = () => {
-  //     // if (items.length == 0) {
-  //     //   return <p>No item Found</p>;
-  //     // }
-  //     items.length === 0 && 'No items found'
-  //   };
+
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <>
       <h1>Pedro</h1>
       {items.length === 0 && "No items found"}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("clicked", item)}
-          >
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
