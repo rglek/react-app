56ed3a1008b7235c17f831ba61e1a07a2bf1d552
Rost Glek <Rost.Glek@islandhealth.ca>
2024-02-03 09:47:12 -0800

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Message from './Message'
-import ListGroup from './components/ListGroup'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+let items = ["New York", "London", "Paris", "Tokio", "Sydney"];
 
-const App =() => {
-  return <div><ListGroup/></div>
-}
+const App = () => {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const handleButtonClick = () => {
+    console.log("click");
+  };
+  return (
+    
+    <div>
+      {}
+      <Alert>
+        <h1>Pedro</h1>
+      </Alert>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Button onClick={handleButtonClick}>Submit</Button>
+      <Button onClick={handleButtonClick} color="danger">
+        Cancel
+      </Button>
+    </div>
+  );
+};
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,35 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
-const ListGroup = () => {
-  let items = ["New York", "London", "Paris", "Tokio", "Sydney"];
-
+const ListGroup = ({items, heading, onSelectItem}: Props) => {
   const handleClick = (event: MouseEvent) => {
     console.log(event);
   };
 
+  const [selectedIndex, setSelectedInex] = useState(-1);
+
   return (
     <>
-      <h1>Pedro</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && "No items found"}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedInex(index);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
